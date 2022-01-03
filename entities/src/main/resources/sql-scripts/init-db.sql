491ddc74a527fca0b749435c33ac120283f45ca0
Gal petkovšek <gp1914@student.uni-lj.si>
2022-01-03 12:55:11 +0100

    sql id-s

--- entities/src/main/resources/sql-scripts/init-db.sql
+++ entities/src/main/resources/sql-scripts/init-db.sql
@@ -9,8 +9,8 @@ INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VAL
 INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (13, 'ice screws', 'ice climbing ice screws', 5, 3, 'ice climbing');
 INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (15, 'chalk bag', 'climbing chalk bag', 3, 2, 'rock climbing');
 INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (16, 'helmet', 'climbing helmet', 5, 3, 'climbing');
-INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (15, 'bag', 'hiking bag', 8, 4, 'hiking');
-INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (16, 'hiking sticks', 'hiking sticks', 5, 3, 'hiking');
+INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (17, 'bag', 'hiking bag', 8, 4, 'hiking');
+INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (18, 'hiking sticks', 'hiking sticks', 5, 3, 'hiking');
 INSERT INTO event (id_event, title, description, start_date, end_date, id_persone, tag) VALUES (21, 'Climbing school for beginners', 'Basics of climbing', '2022-05-05', '2022-06-06', 1, 'climbing');
 INSERT INTO event (id_event, title, description, start_date, end_date, id_persone, tag) VALUES (22, 'Climbing school 2', 'Climbing school for those who know basics', '2022-07-01', '2022-09-01', 4, 'climbing');
 INSERT INTO event (id_event, title, description, start_date, end_date, id_persone, tag) VALUES (23, 'Climbing seminar', 'Seminar of climbing in America', '2022-08-05', '2022-08-06', 5, 'climbing');
